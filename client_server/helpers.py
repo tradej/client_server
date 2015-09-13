aa9a997183edf6ef1d656f9e94053855e2bf437d
Miro Hrončok <miro@hroncok.cz>
2015-09-13 21:26:06 +0200

    Also return assistants in tree calls (no icons yet)

--- client_server/helpers.py
+++ client_server/helpers.py
@@ -1,11 +1,24 @@
 from six import string_types
 
 from devassistant import actions
+from devassistant import bin
 
 from client_server import exceptions
 
 def get_tree(depth, icons, root):
-    tree = get_all_actions(depth, icons, root, prefix=root + '/')
+    treevalid = True
+    tree = []
+    try:
+        tree = get_actions(depth, icons, root, prefix=root + '/')
+    except exceptions.ProcessingError:
+        treevalid = False
+
+    try:
+        tree += get_assistants(depth, icons, root, prefix=root + '/')
+    except exceptions.ProcessingError as e:
+        if not treevalid:
+            raise(e)
+
     return {'tree': tree}
 
 def get_detail(*args, **kwargs):
@@ -15,9 +28,9 @@ def run_runnable(*args, **kwargs):
     raise exceptions.ProcessingError('Not Implemented')
 
 
-def get_all_actions(depth, icons, root, currentdepth=1, prefix=''):
+def get_actions(depth, icons, root, currentdepth=1, prefix=''):
     tree = []
-    root = get_root(root)
+    root = get_actions_root(root)
 
     for action in root:
         if action.hidden:
@@ -33,7 +46,7 @@ def get_all_actions(depth, icons, root, currentdepth=1, prefix=''):
         }
 
         if not depth or depth > currentdepth:
-            a['children'] = get_all_actions(depth, icons, root=action,
+            a['children'] = get_actions(depth, icons, root=action,
                                             currentdepth=currentdepth+1,
                                             prefix=a['path'] + '/')
 
@@ -41,7 +54,7 @@ def get_all_actions(depth, icons, root, currentdepth=1, prefix=''):
     return tree
 
 
-def get_root(root):
+def get_actions_root(root):
     if not root:
         return actions.actions
 
@@ -59,3 +72,48 @@ def get_root(root):
                 raise exceptions.ProcessingError('Invalid root')
 
     return root.get_subactions()
+
+
+# TODO get_{assistants,actions}() are almost teh same, unify somehow?
+
+def get_assistants(depth, icons, root, currentdepth=1, prefix=''):
+    tree = []
+    root = get_assistants_root(root)
+
+    for assistant in root:
+        a = {
+            'name': assistant.name,
+            'fullname': assistant.fullname,
+            'description': assistant.description,
+            'path': prefix + assistant.name,
+            'icon': None,  # actions currently have no icons
+            'args': [a.__dict__ for a in assistant.args],
+        }
+
+        if not depth or depth > currentdepth:
+            a['children'] = get_assistants(depth, icons, root=assistant,
+                                           currentdepth=currentdepth+1,
+                                           prefix=a['path'] + '/')
+
+        tree.append(a)
+    return tree
+
+def get_assistants_root(root):
+    if not root:
+        top = bin.TopAssistant()
+        return top.get_subassistants()
+
+    if isinstance(root, string_types):
+        path = root.split('/')
+        root = bin.TopAssistant()
+        for segment in path:
+            found = False
+            for candidate in root.get_subassistants():
+                if candidate.name == segment:
+                    root = candidate
+                    found = True
+                    break
+            if not found:
+                raise exceptions.ProcessingError('Invalid root')
+
+    return root.get_subassistants()
