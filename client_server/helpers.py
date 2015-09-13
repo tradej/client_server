f2e7dde71443ac19fea85039878129fd13fd4f88
Miro Hrončok <miro@hroncok.cz>
2015-09-13 18:35:42 +0200

    Return actions in tree requests

--- client_server/helpers.py
+++ client_server/helpers.py
@@ -1,11 +1,60 @@
+from six import string_types
+
+from devassistant import actions
 
 from client_server import exceptions
 
 def get_tree(depth, icons, root):
-    raise exceptions.ProcessingError('Not Implemented')
+    tree = get_all_actions(depth, icons, root, prefix=root + '/')
+    return {'tree': tree}
 
 def get_detail(*args, **kwargs):
     raise exceptions.ProcessingError('Not Implemented')
 
 def run_runnable(*args, **kwargs):
     raise exceptions.ProcessingError('Not Implemented')
+
+
+def get_all_actions(depth, icons, root, currentdepth=1, prefix=''):
+    tree = []
+    root = get_root(root)
+
+    for action in root:
+        if action.hidden:
+            continue
+
+        a = {
+            'name': action.name,
+            'fullname': action.name,
+            'description': action.description,
+            'path': prefix + action.name,
+            'icon': None,  # actions currently have no icons
+        }
+
+        if not depth or depth > currentdepth:
+            a['children'] = get_all_actions(depth, icons, root=action,
+                                            currentdepth=currentdepth+1,
+                                            prefix=a['path'] + '/')
+
+        tree.append(a)
+    return tree
+
+
+def get_root(root):
+    if not root:
+        return actions.actions
+
+    if isinstance(root, string_types):
+        path = root.split('/')
+        root = actions.actions
+        for segment in path:
+            found = False
+            for candidate in root:
+                if candidate.name == segment:
+                    root = candidate
+                    found = True
+                    break
+            if not found:
+                raise exceptions.ProcessingError('Invalid root')
+
+    return root.get_subactions()
